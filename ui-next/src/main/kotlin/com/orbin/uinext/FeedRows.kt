bf0af88a98dc7444febbd7ff41777a1d42b09fe3
Defuuls <295269552+Defuuls@users.noreply.github.com>
2026-09-17 14:19:44 -0500

    Fix UI lint imports

--- ui-next/src/main/kotlin/com/orbin/uinext/FeedRows.kt
+++ ui-next/src/main/kotlin/com/orbin/uinext/FeedRows.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
